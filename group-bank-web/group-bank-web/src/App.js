b81f5ecbd253f4459b3e71cec9f2a89dfbff0422
pauliepj <61068475+pauliepj@users.noreply.github.com>
2021-06-16 14:24:35 -0500

    INCREDIBLY painful second commit.
    AccountDataService works now, as does the axios get method.

---
+++ group-bank-web/group-bank-web/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import LoginPage from './login-page/LoginPage';
+import HomePage from './home-page/HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Account from './components/Account';
+import Profile from './components/Profile';
+import React, { useEffect } from 'react';
+import axios from "axios";
+
+
+
+
+
+
+class App extends React.Component{
+  
+  
+  render(){
+  return (
+    <div className="App">
+      
+      <Router>
+        
+      <Header/>
+      <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/login" component={LoginPage}/>
+      <Route exact path="/accounts" component={Account}/>
+      <Route exact path="/profile" component={Profile}/>
+
+      </Switch>
+      <Footer/>
+      </Router>
+      
+    </div>
+  );
+  }
+
+}
+
+export default App;
---
+++ group-bank-web/group-bank-web/src/components/Footer.js
@@ -0,0 +1,14 @@
+import "../style/footer.css";
+function Footer() {
+    
+    return(
+        <footer className="fixed-bottom font-small bg-primary">
+
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+          <p> UltraGroupBank </p>
+        </div>
+      
+      </footer>
+    );
+}
+export default Footer;
